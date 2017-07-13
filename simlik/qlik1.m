26d2fc6a8ef74369d3b9b43aade2b1d6db05b9cf
Will Moore <neurd@icloud.com>
2017-07-13 11:00:51 -0400

    add simlik

---
+++ simlik/qlik1.m
@@ -0,0 +1,86 @@
+function [lik, data] = qlik1(x,data,sim,expt)
+    
+    % Q-learning on multi-armed bandit
+    %
+    % USAGE: [lik, data] = qlik1(x,data,opts)
+    %
+    % INPUTS:
+    %   x - parameters: (may vary based on opts structure)
+    %       x(1) - inverse temperature
+    %       x(2) - stickiness inverse temperature
+    %       x(3) - learning rate
+    %
+    %
+    %   data - structure with the following fields:
+    %       .c - [N x 1] choices
+    %       .r - [N x 2] rewards [gems, bomb]
+    %       .gP - [N x C] gems reward probabilities
+    %       .bP - [N x C] bomb reward probabilities
+    %       .C - number of choice options
+    %       .N - number of trials
+    %       .O - number of reward types
+    %       .D - probability door returns gems [only for non-simultaneous gems/bomb outcomes]
+    %
+    %
+    % OUTPUTS:
+    %   lik - log-likelihood
+    %
+    %   data - structure with the following fields:
+    %   %-------likelihood mode-------%
+    %           .v - [N x C] learned values
+    %           .rpe - [N x O] reward prediction error for chosen option
+    %   %-------simulation mode-------%
+    %           .c - [N x 1] choices
+    %           .r - [N x 1] rewards
+    %
+    % Sam Gershman, July 2015
+    
+    it = x(1);
+    k  = x(2);
+    lr = x(3);
+
+    C = data.C;
+    v = zeros(1,C); % initial values
+    u = zeros(1,C);  % stickiness
+    lik = 0;
+    for n = 1:data.N
+        q = it*v + k*u;
+        % simulation mode
+        if sim == 1
+            p = exp(q - logsumexp(q,2));
+            c = fastrandsample(p);
+            lik = lik + log(p(c));
+            r = [0 0];
+            if (strcmp(expt,'b2') || strcmp(expt,'b3'))
+                % determine rewards
+                if data.gP(n,c) > rand
+                    if data.D(c) > rand
+                        r(1) = 1;
+                    else
+                        r(2) = 1;
+                    end
+                end
+            elseif (strcmp(expt,'b4') || strcmp(expt,'b1'))
+                if data.gP(n,c) > rand
+                    r(1) = 1;
+                end
+                if data.bP(n,c) > rand
+                    r(2) = 1;
+                end
+            end
+            data.c(n,1) = c;
+            data.r(n,:) = r;
+        % likelihood mode
+        else
+            c = data.c(n);
+            r = data.r(n,:);
+            lik = lik + q(c) - logsumexp(q,2);
+        end
+        % compute gems/bomb reward prediction errors
+        rpe = sum(r)-v(c);
+        % update values
+        v(c) = v(c) + lr*rpe;
+        % update stickiness
+        u = zeros(1,C); u(c) = 1;
+        data.rpe(n,1) = rpe;
+    end
