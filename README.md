52d93cc86299288120943607ed1bad7fdd85a52d
Will Moore <neurd@icloud.com>
2017-07-17 11:25:04 -0400

    modified ppc_raw_b1 to sort gP/bP correctly

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # gems_vs_bomb
 code to analyze intergroup bandit task 
+
+memo to myself:  
+you can estimate pGems/pBomb on each trial & compare to ground truth 
+
+It's dark in the cave, and you can't tell whether the other person is a Nyx or a Zur. Both go gems, max payout. Both go sabotage, they cancel out. One and one gems guy gets kaboomed.
--- munge/ppc_raw_b1.m
+++ munge/ppc_raw_b1.m
@@ -1,20 +1,18 @@
-function [data, demo] = ppc_raw_b1(studyDir)
+function [data] = ppc_raw_b1(studyDir)
 % ppc_raw_b1.M %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % Preprocess b1_single data for model specification via mfit
 %
-% USAGE: [data, demo] = ppc_raw_b1(studyDir)
+% USAGE: [data] = ppc_raw_b1(studyDir)
 %
 % INPUT:
 %       studyDir = path to top level of git repo [string]
 %
 % OUTPUT:
 %       data = data structure w/ correct fields for mfit
-%       demo = table object w/ per subject data
-%
 %
 % written: ~#wem3#~ [20170203]
-% edited:  ~#wem3#~ [20170207]
+% edited:  ~#wem3#~ [20170713]
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % set up paths
@@ -22,14 +20,17 @@ rawDataDir = fullfile(studyDir,'raw_data','bandit_single','task');
 cleanDataDir = fullfile(studyDir,'data_analysis','bandit_single');
 codeDir = fullfile(cleanDataDir,'scripts');
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% munge
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % get demographic data (cat raw data, write temp .csv & read in)
 cd(rawDataDir);
-system(['cat *.csv | grep "survey-text-sam" | sed ''s/{.*,.*}//g'' | cut -d'','' -f 7,10,33,36,38 | sed ''s/"//g'' > ', fullfile(cleanDataDir,'b1_demo.csv')]);
+system(['cat *.csv | grep "survey-text-sam" | sed ''s/{.*,.*}//g'' | cut -d'','' -f 7,10,33,36,38 | sed ''s/"//g'' > ', fullfile(cleanDataDir,'b1_subject_data.csv')]);
 b1_demo = readtable(fullfile(cleanDataDir,'b1_demo.csv'),'Format','%s%s%d%s%s');
 b1_demo.Properties.VariableNames = {'subID','cond','age','gender','race'};
 b1_demo.cond(strcmp(b1_demo.cond,'pain'))={'bomb'};
 b1_demo.cond(strcmp(b1_demo.cond,'gain'))={'gems'};
-system(['rm ',fullfile(cleanDataDir,'b1_demo.csv')]);
+% system(['rm ',fullfile(cleanDataDir,'b1_subject_data.csv')]);
 
 % get manipulation check data (cat raw data, write temp .csv & read in)
 system(['cat *.csv | grep "0.0-20.0-0.0" | sed ''s/{.*,.*}//g'' | cut -d'','' -f 7,15 | sed ''s/"//g'' > ', fullfile(cleanDataDir,'mCheck.csv')]);
@@ -37,8 +38,12 @@ mCheck = csvread(fullfile(cleanDataDir,'mCheck.csv'));
 b1_demo.failMC = mCheck(:,2);
 system (['rm ',(fullfile(cleanDataDir,'mCheck.csv'))]);
 % get per trial data
-system(['cat *.csv | grep main_expt | cut -d'','' -f 1,7,11,15,17,18,20,21,23,24,25,27,28,29,30,31,32 | sed ''s/"//g'' > ', fullfile(cleanDataDir,'b1_data.csv')]);
-tmp = csvread(fullfile(cleanDataDir,'b1_data.csv'));
+system(['cat *.csv | grep main_expt | cut -d'','' -f 1,7,11,15,17,18,20,21,23,24,25,27,28,29,30,31,32 | sed ''s/"//g'' > ', fullfile(cleanDataDir,'b1_trial_data.csv')]);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% preprocess for mfit
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+tmp = csvread(fullfile(cleanDataDir,'b1_trial_data.csv'));
 subID = tmp(:,2);
 drift = tmp(:,3);
 tnum = tmp(:,9);
@@ -47,27 +52,37 @@ reward = [tmp(:,5)+tmp(:,6)];
 rt = tmp(:,1);
 pChoice = tmp(:,7)+tmp(:,8);
 pArms = [tmp(:,10)+tmp(:,11),tmp(:,12)+tmp(:,13),tmp(:,14)+tmp(:,15),tmp(:,16)+tmp(:,17)];
-system(['rm ',fullfile(cleanDataDir,'b1_data.csv')]);
-cd(cleanDataDir);
+%system(['rm ',fullfile(cleanDataDir,'b1_data.csv')]);
 subList=str2num(char(b1_demo.subID));
 
 % loop over b1_trial
-for s = 1:height(b1_demo)
+for s = 1:length(subList)
     % for data structure to use w/ mfit
-    d.subID = b1_demo.subID(s);
-    d.cond = b1_demo.cond(s);
-    d.drift = unique(drift(subID==subList(s)));
+    subject.subID = b1_demo.subID(s);
+    subject.cond = b1_demo.cond(s);
+    subject.age = b1_demo.age(s);
+    subject.sex = b1_demo.gender(s);
+    subject.race = b1_demo.race(s);
+    subject.drift = unique(drift(subID==subList(s)));
     d.N = length(choice(subID==subList(s)));
     d.C = length(unique(choice(subID==subList(s))));
     d.c = choice(subID==subList(s));
-    d.r = reward(subID==subList(s));
+    d.r = zeros(d.N,2); % placeholder so we can make structures identical
     d.rt = rt(subID==subList(s));
-    d.p = pArms(subID==subList(s),:);
+    % only needed for b1
+    if strcmp(b1_demo.cond,'gems')
+        d.r(:,1) = reward(subID==subList(s));
+        d.pG = pArms(subID==subList(s),:);
+        d.pB = zeros(size(pArms));
+    elseif strcmp(b1_demo.cond,'bomb')
+        d.r(:,2) = reward(subID==subList(s));
+        d.pG = pArms(subID==subList(s),:);
+        d.pB = zeros(size(pArms));
+    end
+    d.subject = subject;
     data(s) = d;
     % update b1_demo w/ payout, meanRT, totalRT
-    b1_demo.payout(s) = sum(d.r);
-    b1_demo.meanRT(s) = mean(d.rt);
-    b1_demo.totalRT(s) = sum(d.rt);
+    % b1_demo.payout(s) = sum(d.r);
+    % b1_demo.meanRT(s) = mean(d.rt);
+    % b1_demo.totalRT(s) = sum(d.rt);
 end
-demo = b1_demo;
-%cd(cleanDataDir);
