732957b5db29eec1f48d9a5535e7dcd4ccd52152
Will Moore <neurd@icloud.com>
2017-07-18 18:45:25 -0400

    fixed stupid typo ARGH

--- simlik/set_opts.m
+++ simlik/set_opts.m
@@ -23,21 +23,21 @@ function param = set_opts(model)
 % params 1 - 3 are the same for all models
 param(1).name = 'inverse temperature'; % inverse temperature
 param(1).hp = [3 2];    % hyperparameters of the gamma prior
-param(1).lopGdf = @(x) sum(log(gampdf(x,param(1).hp(1),param(1).hp(2))));  % log density function for prior
+param(1).logpdf = @(x) sum(log(gampdf(x,param(1).hp(1),param(1).hp(2))));  % log density function for prior
 param(1).lb = 1e-8; % lower bound
 param(1).ub = 50;   % upper bound
 param(1).fit = @(x) gamfit(x);
 
 param(2).name = 'choice stickiness'; % perseveration parameter
 param(2).hp = [0 10]; % hyperparameters of the normal prior
-param(2).lopGdf = @(x) sum(log(normpdf(x,param(2).hp(1),param(2).hp(2))));  % log density function for prior
+param(2).logpdf = @(x) sum(log(normpdf(x,param(2).hp(1),param(2).hp(2))));  % log density function for prior
 param(2).lb = -5;    % lower bound
 param(2).ub = 5;     % upper bound
 param(2).fit = @(x) [mean(x) std(x)];
 
 param(3).name = 'learning rate'; % learning rate
 param(3).hp = [1.2 1.2];    % hyperparameters of beta prior
-param(3).lopGdf = @(x) sum(log(betapdf(x,param(3).hp(1),param(3).hp(2))));
+param(3).logpdf = @(x) sum(log(betapdf(x,param(3).hp(1),param(3).hp(2))));
 param(3).lb = 0;
 param(3).ub = 1;
 param(3).fit = @(x) betafit(x);
